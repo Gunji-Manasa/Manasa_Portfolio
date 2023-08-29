0316da94f1a58a78d4fd8a0610145379351326cc
Gunji-Manasa <gunjimanasa24@gmail.com>
2023-08-29 19:52:52 +0530

    fewchanges

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,15 +9,15 @@ function Card(){
     <Container>
     <Row><center>
     <h1 className="FunTitle">Play and have fun!</h1>
-    <h2 style={{color:"white",paddingBottom:"60px",paddingTop:"20px"}} >Here are a few <b style={{color:"#F11A7B"}}>works</b> I've done recently.</h2>
+    <h2 style={{color:"white",paddingBottom:"60px",paddingTop:"20px"}} >Here are a few <b style={{color:"#F31559"}}>works</b> I've done recently.</h2>
     </center></Row>
     <Row className="fun-row">
     <Col md={4} xs={12} lg={4} className="fun-card">
     <div class="card" style={{width: "18rem"}}>
   <img src={Drums_Image} class="card-img-top" alt="drum-pic"/>
-  <div class="card-body">
+  <div class="card-body" >
     <h5 class="card-title">Drum-Kit</h5>
-    <p class="card-text">It is a Musical fun through online. It can be played by using mouse clicks or keyboard buttons respectively.</p>
+    <p class="card-text">It is a musical fun containing various instruments which can be played by using mouse clicks or keyboard</p>
     <div style={{textAlign:"center"}}>
     <a href="https://gunji-manasa.github.io/drum-kit/" className="card-button" class="btn">Have Fun</a>
     </div>
