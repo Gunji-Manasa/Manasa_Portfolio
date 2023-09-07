e4c6602b893570b4ecb4ec9c2d7fb6db146c7eac
Gunji-Manasa <gunjimanasa24@gmail.com>
2023-09-07 19:20:12 +0530

    fewchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import TagFacesIcon from '@mui/icons-material/TagFaces';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import FeedbackOutlinedIcon from '@mui/icons-material/FeedbackOutlined';
-function Navbar(){
- 
+// import { act } from '@testing-library/react';
+function Navbar() {
   return(
-    <nav class="navbar navbar-expand-lg fixed-top" >
+    <nav class="navbar navbar-expand-lg fixed-top"  >
   <div class="container-fluid">
     {/* <a class="navbar-brand" href="#">Navbar</a> */}
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" style={{color:"white"}}>
